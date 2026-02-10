f42d061a5df89e5dc524e5032e41affb1779469e
DEVERIA, VAIBHAV (Contractor-NBCUniversal) <VAIBHAV.DEVERIA@nbcuni.com>
2026-02-10 09:55:51 -0500

    StringTest.java

---
+++ StringTest.java
@@ -0,0 +1,15 @@
+package qa_java_program;
+
+public class StringTest {
+
+	public static String s1 = "vaibhav";
+	public String s2 = "RAM";
+	
+	public static void main(String[]args) {
+		
+		System.out.println(s1);
+		if(s1.contains("b")) {
+			System.out.println("Test is passed" );
+		}
+	}
+}
